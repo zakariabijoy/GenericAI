a03223c87b08a54dac90b36cf80ee3cd5c3263d0
ZAKARIA BIJOY <zakaria.bijoy@live.com>
2025-06-03 03:44:50 +0600

    Create Catalog Microservice Web API and Add .NET Aspire Orchestration

--- GenericAI/AppHost/Program.cs
+++ GenericAI/AppHost/Program.cs
@@ -2,4 +2,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 //add projects and cloud-native backing services here
 
+builder.AddProject<Projects.Catalog>("catalog");
+
+
 builder.Build().Run();
