998c069d75000e5b418c3b1d8098c55bfedfa1aa
ZAKARIA BIJOY <zakaria.bijoy@live.com>
2025-06-06 04:03:27 +0600

    Catalog Client Integration: Connect PostgreSQL DB in Microservice

--- GenericAI/Catalog/Program.cs
+++ GenericAI/Catalog/Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.AddServiceDefaults();
 
+builder.AddNpgsqlDbContext<ProductDbContext>(connectionName: "catalogDb");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
