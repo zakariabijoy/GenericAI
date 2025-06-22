fc336bf538d8bab3e850986a5f71def34bdff7b0
ZAKARIA BIJOY <zakaria.bijoy@live.com>
2025-06-23 02:28:37 +0600

    Develop Products UpdateProduct and DeleteProduct using ProductDbContext

--- GenericAI/Catalog/Services/ProductService.cs
+++ GenericAI/Catalog/Services/ProductService.cs
@@ -7,4 +7,22 @@ public class ProductService(ProductDbContext dbContext)
         await dbContext.Products.AddAsync(product);
         await dbContext.SaveChangesAsync();
     }
+
+    public async Task UpdateProductAsync(Product updatedProduct, Product inputProduct)
+    {
+        // uodate product with new values
+        updatedProduct.Name = inputProduct.Name;
+        updatedProduct.Description = inputProduct.Description;
+        updatedProduct.ImageUrl = inputProduct.ImageUrl;
+        updatedProduct.Price = inputProduct.Price;
+
+        dbContext.Products.Update(updatedProduct);
+        await dbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteProductAsync(Product product)
+    {
+        dbContext.Products.Remove(product);
+        await dbContext.SaveChangesAsync();
+    }
 }
