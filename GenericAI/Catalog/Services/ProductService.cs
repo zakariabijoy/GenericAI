4b5b9b228c8d0867f519c78e39090f71cb3dc190
ZAKARIA BIJOY <zakaria.bijoy@live.com>
2025-06-23 02:24:30 +0600

    Catalog Application Use Case Development

---
+++ GenericAI/Catalog/Services/ProductService.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog.Services;
+
+public class ProductService(ProductDbContext dbContext)
+{
+    public async Task CreateProductAsync(Product product)
+    {
+        await dbContext.Products.AddAsync(product);
+        await dbContext.SaveChangesAsync();
+    }
+}
