737ac0e34807dcbf878edd2499bd1451f2203441
ZAKARIA BIJOY <zakaria.bijoy@live.com>
2025-06-23 02:31:17 +0600

    Develop Product Queries using ProductDbContext

--- GenericAI/Catalog/Services/ProductService.cs
+++ GenericAI/Catalog/Services/ProductService.cs
@@ -2,6 +2,16 @@
 
 public class ProductService(ProductDbContext dbContext)
 {
+    public async Task<IEnumerable<Product>> GetProductsAsync()
+    {
+        return await dbContext.Products.ToListAsync();
+    }
+
+    public async Task<Product?> GetProductByIdAsync(Guid id)
+    {
+        return await dbContext.Products.FindAsync(id);
+    }
+
     public async Task CreateProductAsync(Product product)
     {
         await dbContext.Products.AddAsync(product);
