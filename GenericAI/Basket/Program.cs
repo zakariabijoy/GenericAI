b08f0bd03b92a7a2f8c1931b8e080b5e2fe1c04b
ZAKARIA BIJOY <zakaria.bijoy@live.com>
2025-06-25 01:54:27 +0600

    Basket Client Integration: Connect Redis Cache in Microservice
    Add Redis distributed caching support
    
    Updated `Basket.csproj` to include `Aspire.StackExchange.Redis.DistributedCaching` package version `9.3.0`.
    Modified `Program.cs` to configure Redis distributed caching with a connection name of `"cache"`.

--- GenericAI/Basket/Program.cs
+++ GenericAI/Basket/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.AddServiceDefaults();
+builder.AddRedisDistributedCache(connectionName: "cache");
 
 var app = builder.Build();
 
