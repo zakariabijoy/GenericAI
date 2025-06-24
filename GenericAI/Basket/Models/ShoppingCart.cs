c9ad60cdb79b1c856b85142d99e85207b3f61d4e
ZAKARIA BIJOY <zakaria.bijoy@live.com>
2025-06-25 01:37:13 +0600

    Develop SC Domain Entity Models in Basket Microservice Domain Models

---
+++ GenericAI/Basket/Models/ShoppingCart.cs
@@ -0,0 +1,8 @@
+﻿namespace Basket.Models;
+
+public class ShoppingCart
+{
+    public string UserName { get; set; }
+    public List<ShoppingCartItem> Items { get; set; }
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+}
---
+++ GenericAI/Basket/Models/ShoppingCartItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Basket.Models;
+
+public class ShoppingCartItem
+{
+    public int Quantity { get; set; } = default!;
+    public string Color { get; set; } = default!;
+    public int ProductId { get; set; } = default!;
+
+    // will come from Catalog module
+    public decimal Price { get; set; } = default!;
+    public int ProductName { get; set; } = default!;
+}
