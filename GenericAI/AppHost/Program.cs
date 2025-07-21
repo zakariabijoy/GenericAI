601ce7acf2e234c3b04341805bd62585d7ce490a
ZAKARIA BIJOY <zakaria.bijoy@live.com>
2025-07-22 00:50:21 +0600

    RabbitMQ Hosting Integration .NET Aspire

--- GenericAI/AppHost/Program.cs
+++ GenericAI/AppHost/Program.cs
@@ -1,3 +1,4 @@
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 //backing services 
@@ -15,16 +16,26 @@ var cache = builder
         .WithDataVolume()
         .WithLifetime(ContainerLifetime.Persistent);
 
+var rabbitMq = builder
+        .AddRabbitMQ("rabbitmq")
+        .WithManagementPlugin()
+        .WithDataVolume()
+        .WithLifetime(ContainerLifetime.Persistent);
+
 //Projects
 var catalog = builder
         .AddProject<Projects.Catalog>("catalog")
         .WithReference(catalogDb)
-        .WaitFor(catalogDb);
+        .WithReference(rabbitMq)
+        .WaitFor(catalogDb)
+        .WaitFor(rabbitMq);
 
 var basket = builder
         .AddProject<Projects.Basket>("basket")
         .WithReference(cache)
         .WithReference(catalog)
-        .WaitFor(cache);
+        .WithReference(rabbitMq)
+        .WaitFor(cache)
+        .WaitFor(rabbitMq);
 
 builder.Build().Run();
