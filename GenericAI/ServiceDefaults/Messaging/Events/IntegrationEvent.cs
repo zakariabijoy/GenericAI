de8c05e10fdaa8362cb017ecc8c32c058941a5ab
ZAKARIA BIJOY <zakaria.bijoy@live.com>
2025-07-22 01:10:41 +0600

    Create Shared Messaging Folders and Classes for ProductPriceChangedIntegration

---
+++ GenericAI/ServiceDefaults/Messaging/Events/IntegrationEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace ServiceDefaults.Messaging.Events;
+
+public record IntegrationEvent
+{
+    public Guid EventId => Guid.NewGuid();
+    public DateTime OccurredOn  => DateTime.Now;
+    public string EventType => GetType().AssemblyQualifiedName;
+}
---
+++ GenericAI/ServiceDefaults/Messaging/Events/ProductPriceChangedIntegrationEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace ServiceDefaults.Messaging.Events;
+
+public record ProductPriceChangedIntegrationEvent : IntegrationEvent
+{
+    public int ProductId { get; init; } = default!;
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public decimal Price { get; set; } = default!;
+    public string ImageUrl { get; set; } = default!;
+}
