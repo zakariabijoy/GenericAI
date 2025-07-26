728dd3c7bc3df2b08315187f0c8d9200c14b1c4d
ZAKARIA BIJOY <zakaria.bijoy@live.com>
2025-07-27 03:48:35 +0600

    Develop MassTransit Extention Methods to Register RabbitMQ connection

---
+++ GenericAI/ServiceDefaults/Messaging/MassTransitExtensions.cs
@@ -0,0 +1,35 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace ServiceDefaults.Messaging;
+
+public static class MassTransitExtensions
+{
+    public static IServiceCollection AddMassTransitWithAssemblies(this IServiceCollection services, params Assembly[] assemblies)
+    {
+        services.AddMassTransit(config =>
+        {
+            config.SetKebabCaseEndpointNameFormatter();
+
+            config.SetInMemorySagaRepositoryProvider();
+
+            config.AddConsumers(assemblies);
+            config.AddSagaStateMachines(assemblies);
+            config.AddSagas(assemblies);
+            config.AddActivities(assemblies);
+
+            config.UsingRabbitMq((context, cfg) =>
+            {
+                var configuration = context.GetRequiredService<IConfiguration>();
+                var connectionString = configuration.GetConnectionString("rabbitmq");
+
+                cfg.Host(connectionString);
+                cfg.ConfigureEndpoints(context);
+
+            });
+        });
+        return services;
+    }
+}
