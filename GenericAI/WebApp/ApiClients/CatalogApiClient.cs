db6af56bd3ff32ca0abe7d828ac5cb052bf716d7
ZAKARIA BIJOY <zakaria.bijoy@live.com>
2025-08-02 03:15:59 +0600

    Client WebApp integrate to Catalog w/ CatalogApiClient.cs

---
+++ GenericAI/WebApp/ApiClients/CatalogApiClient.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.Models;
+
+namespace WebApp.ApiClients;
+
+public class CatalogApiClient(HttpClient httpClient)
+{
+    public async Task<List<Product>> GetProducts()
+    {
+        var response = await httpClient.GetFromJsonAsync<List<Product>>($"/products");
+        return response!;
+    }
+
+    public async Task<Product> GetProductById(int id)
+    {
+        var response = await httpClient.GetFromJsonAsync<Product>($"/products/{id}");
+        return response!;
+    }
+}
