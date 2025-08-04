ee71aaec4324bc7b94c21914ab923f3d122a1c19
ZAKARIA BIJOY <zakaria.bijoy@live.com>
2025-08-05 02:49:24 +0600

    Ollama and Llama Model Hosting Integration .NET Aspire

--- GenericAI/AppHost/Program.cs
+++ GenericAI/AppHost/Program.cs
@@ -32,13 +32,23 @@ if (builder.ExecutionContext.IsRunMode)
     keycloak.WithDataVolume();
 }
 
+var ollama = builder
+        .AddOllama("ollama", 11434)
+        .WithDataVolume()
+        .WithLifetime(ContainerLifetime.Persistent)
+        .WithOpenWebUI();
+
+var llama = ollama.AddModel("llama3.2");
+
 //Projects
 var catalog = builder
         .AddProject<Projects.Catalog>("catalog")
         .WithReference(catalogDb)
         .WithReference(rabbitMq)
+        .WithReference(llama)
         .WaitFor(catalogDb)
-        .WaitFor(rabbitMq);
+        .WaitFor(rabbitMq)
+        .WaitFor(llama);
 
 var basket = builder
         .AddProject<Projects.Basket>("basket")
