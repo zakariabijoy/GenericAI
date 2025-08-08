67d1e59028ff23f3c70d739a1664328481b4cbc0
ZAKARIA BIJOY <zakaria.bijoy@live.com>
2025-08-09 03:35:22 +0600

    Develop ProductVector Domain Entity for Storing Vector Data

---
+++ GenericAI/Catalog/Models/ProductVector.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.VectorData;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Catalog.Models;
+
+public class ProductVector
+{
+    [VectorStoreKey]
+    public int Id { get; set; }
+    [VectorStoreData]
+    public string Name { get; set; } = default!;
+    [VectorStoreData]
+    public string Description { get; set; } = default!;
+    [VectorStoreData]
+    public decimal Price { get; set; }
+    [VectorStoreData]
+    public string ImageUrl { get; set; } = default!;
+
+    [NotMapped]
+    [VectorStoreVector(384, DistanceFunction = DistanceFunction.CosineSimilarity)]
+    public ReadOnlyMemory<float> Vector { get; set; }
+}
