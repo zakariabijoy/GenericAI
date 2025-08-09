2edb6ae4057068a2158b68c062b5a9583ef54da6
ZAKARIA BIJOY <zakaria.bijoy@live.com>
2025-08-10 03:49:26 +0600

    Blazor FrontEnd Search Page Development

--- GenericAI/WebApp/ApiClients/CatalogApiClient.cs
+++ GenericAI/WebApp/ApiClients/CatalogApiClient.cs
@@ -21,4 +21,12 @@ public class CatalogApiClient(HttpClient httpClient)
         var response = await httpClient.GetFromJsonAsync<string>($"/products/support/{query}");
         return response!;
     }
+
+    public async Task<List<Product>> SearchProducts(string query, bool aiSearch)
+    {
+        if (aiSearch)
+            return await httpClient.GetFromJsonAsync<List<Product>>($"/products/aisearch/{query}") ?? [];
+        else
+            return await httpClient.GetFromJsonAsync<List<Product>>($"/products/search/{query}") ?? [];
+    }
 }
